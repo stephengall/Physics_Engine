aa91eacb355c5f903d4d4719359440608cc14e1d
Stephen Gallagher <stephengallagher54@gmail.com>
2023-02-26 20:01:23 +0000

    .

--- Start_Physics_Engine.sh
+++ Start_Physics_Engine.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-echo "Starting Physics Engine..."
-cd Physics_Engine_Code
-open http://localhost:8000
-php -S localhost:8000
+ps -ef | grep "php -S localhost:8000" | grep -v grep | awk '{print $2}' | xargs kill
+input=""
+
+while [[ "$input" != "Y" ]] && [[ "$input" != "N" ]]
+do
+		read -p "Run server in background? (Y)es/(N)o " input
+done
+
+if [[ "$input" == "Y" ]]; then
+		echo "Starting PHP server in background..."
+		cd Physics_Engine_Code
+		open http://localhost:8000
+		nohup php -S localhost:8000 & 
+elif [[ "$input" == "N" ]]; then 
+		echo "Starting PHP server..."
+		cd Physics_Engine_Code
+		open http://localhost:8000
+		php -S localhost:8000
+fi
 
